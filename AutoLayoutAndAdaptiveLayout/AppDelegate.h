d889478128902e4f2a175ac146ca52574960653b
supratim2009 <FreeUser@Fakes-Mac.local>
2015-05-31 19:53:00 +0530

    First Sync

---
+++ AutoLayoutAndAdaptiveLayout/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AutoLayoutAndAdaptiveLayout
+//
+//  Created by Fake Client on 31/05/15.
+//  Copyright (c) 2015 Fake Client. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AutoLayoutAndAdaptiveLayout/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AutoLayoutAndAdaptiveLayout
+//
+//  Created by Fake Client on 31/05/15.
+//  Copyright (c) 2015 Fake Client. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
